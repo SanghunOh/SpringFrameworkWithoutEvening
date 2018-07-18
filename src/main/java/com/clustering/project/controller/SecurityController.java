5ddcedc6e1cd5af9892f4e1c616ebe462f9e0a61
Sanghunoh <otter.oh@gmail.com>
2018-07-18 11:33:10 +0900

    add function CustomAuthenticationFailHandler

--- src/main/java/com/clustering/project/controller/SecurityController.java
+++ src/main/java/com/clustering/project/controller/SecurityController.java
@@ -3,12 +3,20 @@
  */
 package com.clustering.project.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author ohsanghun
@@ -19,8 +27,14 @@ public class SecurityController {
 
 
 	@RequestMapping(value = "/loginForm", method = {RequestMethod.GET, RequestMethod.POST})
-	public void login(Locale locale, Model model) {
-		model.getClass();
+	public ModelAndView actionMethod(@RequestParam Map<String, Object> paramMap, ModelAndView modelandView) {
+
+		String viewName = "loginForm";
+
+		modelandView.setViewName(viewName);
+
+		modelandView.addObject("resultMap", paramMap);
+		return modelandView;
 	}
 /*
 	@RequestMapping(value = {"/j_spring_security_logout"}, method = RequestMethod.GET)
